5e20587ac57499a557492f1f1a95f08e090c9140
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-11-06 13:13:55 -0500

    send message to second radio if too close

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan
+ * Created on: Oct 2025
+ * This program sends a message throught radio if sonar too close to something.
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(14)
+basic.showIcon(IconNames.Happy)
+
+// send message to second radio if too close
+
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+
+        let distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+
+        if (distance < 10) {
+            radio.sendString("Too close")
+        }
+
+        radio.sendNumber(distance)
+
+        // Added pause to prevent crash
+        basic.pause(500)
+    }
+})
