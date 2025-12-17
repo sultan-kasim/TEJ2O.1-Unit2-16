c3b6eb19406b0ee6b19aa099f202be821fbd391e
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-12-17 12:56:16 -0500

    sends a message throught radio if sonar too close to something.

--- main.ts
+++ main.ts
@@ -8,20 +8,23 @@
 radio.setGroup(15)
 basic.showIcon(IconNames.Happy)
 
+
 basic.forever(function () {
     let distance = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
     )
-//check if that object is too close
-    if (distance > 0) {
-        if (distance < 10) {
-            basic.clearScreen()
-            radio.sendString("Too close")
-            basic.pause(200)
-            basic.showIcon(IconNames.Happy)
-        }
+
+
+    // check if that object is too close
+    if (distance > 0 && distance < 10) {
+        basic.clearScreen()
+        radio.sendString("Too close")
+        basic.pause(200)
+        basic.showIcon(IconNames.Happy)
     }
+
+
     basic.pause(500)
 })
